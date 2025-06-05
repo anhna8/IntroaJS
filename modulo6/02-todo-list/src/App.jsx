d8de84a5dd15c87f6e3cf9dac481db7ca584fe17
anhna <ana_mjl@hotmail.com>
2025-06-04 20:00:05 -0600

    add  inter proyect

--- modulo6/02-todo-list/src/App.jsx
+++ modulo6/02-todo-list/src/App.jsx
@@ -1,11 +1,10 @@
 import './App.css'
 
-function App() {
-
+function App () {
   return (
     <>
       <h1>To-do List</h1>
-      <input type="text" placeholder="Escribe una tarea" />
+      <input type='text' placeholder='Escribe una tarea' />
       <button>Añadir</button>
       <ul>
         <li>Item 1<button>Eliminar</button></li>
